827acf32e325a12f3bc793b3fc646ecae63ba85a
Aaidt <aadits2004@gmail.com>
2025-07-20 16:19:22 +0530

    healthcheck ep

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import express from "express"
 import os from "os";
 
-export const app = express()
+export const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Helo world")
+})
+
 app.get("/cpu", (req, res) => {
     for(let i = 0; i < 100000000000; i++ ){
         Math.random()
