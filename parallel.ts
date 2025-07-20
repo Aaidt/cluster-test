abee00a2f0f22bf36429dfe22a8e087085eed54f
Aaidt <aadits2004@gmail.com>
2025-07-20 20:35:49 +0530

    testing

--- parallel.ts
+++ parallel.ts
@@ -1,48 +1,47 @@
-import cluster from "cluster";
 import * as os from "os";
+import cluster from "cluster";
 
+const count = 1_000_000_000;
 const total_cpus = os.cpus().length;
-const count = 1_000_000_000_0;
 const chunkSize = Math.floor(count / total_cpus);
-let ans = 0
 
 if (cluster.isPrimary) {
-    let total = 0;
+    console.log(`Master process started: ${process.pid}`);
+    const startTime = Date.now();
+
+    let ans = 0;
     let completed = 0;
-    const startTime = Date.now()
 
     for (let i = 0; i < total_cpus; i++) {
         const worker = cluster.fork();
+        console.log(`Worker ${worker.process.pid} started.`);
+
         const start = i * chunkSize;
-        const end = i === total_cpus - 1 ? count : (i + 1) * chunkSize - 1;
+        const end = i === total_cpus - 1 ? count : (i + 1) * chunkSize;
 
         worker.send({ start, end });
 
-        worker.on('message', (msg) => {
-            if (msg.partialSum !== undefined) {
-                total += msg.partialSum
-                completed++
-                if (completed == total_cpus) {
-                    const endTime = Date.now()
-                    console.log('Total sum: ' + total);
-                    console.log(`Total time: ${endTime - startTime}`);
-                    process.exit()
-                }
+        worker.on("message", (msg) => {
+            ans += msg.partialSum;
+            completed++;
+
+            if (completed === total_cpus) {
+                const endTime = Date.now();
+                console.log(`Total sum: ${ans}`);
+                console.log(`Total time taken: ${endTime - startTime} ms`);
+                process.exit();
             }
         });
-
     }
 } else {
-    process.on('message', (msg: { start: number, end: number }) => {
-        const { start, end } = msg
+    process.on("message", (msg: { start: number; end: number }) => {
+        const { start, end } = msg;
         let localSum = 0;
-        const t0 = Date.now();
 
         for (let n = start; n < end; n++) {
-            localSum += n
+            localSum += n;
         }
-        const t1 = Date.now()
+
         if (process.send) process.send({ partialSum: localSum });
-        console.log(`Worker ${process.pid} completed in ${(t1 - t0) / 1000} seconds`)
-    })
-}   
+    });
+}
